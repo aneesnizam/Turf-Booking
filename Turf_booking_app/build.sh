82c03c74db8d61d19273b1a21d11bd96c2c67f35
Anees <aneesnizam222a@gmail.com>
2025-08-23 00:46:55 +0530

    updated

--- Turf_booking_app/build.sh
+++ Turf_booking_app/build.sh
@@ -4,7 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 
 # Add this line to move into your project folder
-cd Turf_booking_app
 
 # These commands will now run correctly
 python manage.py collectstatic --no-input
